db25b1a1ba825db16f580dcf0ba7253a6d4456e4
Tuan Kiet <kiethdt.22ite@vku.udn.vn>
2023-08-26 21:39:24 +0700

    useState(update)

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -4,22 +4,23 @@ import { useState } from 'react'
   const orders = [100,200,300]
 
 function App() {
-  const total = orders.reduce((acc, order) => acc + order)
-  const [counter, setCounter] = useState(()=>{
-    const total = orders.reduce((acc, order) => acc + order)
-    // get values returned to the initial state
-    return total
-  });
-
-  const handleIncrease = () =>{
-    setCounter(prevState => prevState+1)
-    setCounter(prevState => prevState+1)
-    setCounter(prevState => prevState+1)
+  const [info,setInfo] = useState({
+    name : 'orders',
+    age: 17,
+    address : '123 street Dinh Tien Hoan'
+  })
+  const handleUpdate = () => {
+    setInfo({
+      // add info
+    ...info,
+    bio: 'John',
+    })
   }
+
   return (
     <div className="App" style={{padding: 20}}>
-      <h1>Counter: {counter}</h1>
-      <button onClick={handleIncrease}>Increase</button>
+      <h1>{JSON.stringify(info)}</h1>
+      <button onClick={handleUpdate}>handleUpdate</button>
     </div>
   );
 }
