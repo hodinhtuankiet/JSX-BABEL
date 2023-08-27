c1add0ac7ccce715b11d8697355f2ddc8ace94a6
Tuan Kiet <kiethdt.22ite@vku.udn.vn>
2023-08-27 17:13:02 +0700

    two way binding checkbox

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,29 +1,54 @@
 
 import { useState } from 'react'
 
+const courses = [
+  {
+    id: 1,
+    name: 'Course1'
+  },
+  {
+    id: 2,
+    name: 'Course2'
+  },
+  {
+    id: 3,
+    name: 'Course3'
+  },
 
+]
 function App() {
-  
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
+  const [checked,setCheck] = useState([])
 
-  const handleGift = () => {
-    console.log({
-      name,email
+  const handleCheck = (id) =>{
+    setCheck(prev =>{
+      const isChecked = checked.includes(id)
+      if(isChecked) {
+        return checked.filter(item => item !== id)
+      }else{
+        return [...prev,id]
+      }
     })
-    }
-
+  }
+  const handleSumit = () =>{
+    console.log({ ids: checked})
+  }
   return (
     <div className="App" style={{padding: 20}}>
-      <input
-      value={name}
-      onChange={e=> setName(e.target.value)}
-      />
-      <input
-      value={email}
-      onChange={e=> setEmail(e.target.value)}
-      />
-      <button onClick={handleGift}>Send</button>
+     {
+      courses.map((course) => {
+        return (
+          <div key={course.id}>
+            <input 
+            type='checkbox'
+            name='course'
+            checked={checked.includes(course.id)} 
+            onChange={()=> handleCheck(course.id)}
+            />{course.name}
+          </div>
+        )
+      }
+     )}
+      <button onClick={handleSumit} >Send</button>
     </div>
   );
 }
