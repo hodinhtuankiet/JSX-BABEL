853a21806992d8434bd055c7febd50fd2ac1cc74
Tuan Kiet <kiethdt.22ite@vku.udn.vn>
2023-08-30 21:14:13 +0700

    unmounted-mounted

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,36 +1,13 @@
 
 import { useState } from 'react'
-
+import Content from './Content';
 
 function App() {
-  // cause empty array , call variable
-  const storageJobs = JSON.parse(localStorage.getItem('job'))
-  // convert json to array
-  // JSON.parse(storageJobs)
-  // if storageJobs is empty string then return empty array
-  const [jobs,setJobs] = useState(storageJobs ?? [])
-  const [job,setJob] = useState('')
-
- 
-  const handleSumit = () =>{
-    setJobs(prev => {
-      const newJobs = [...prev,job]
-      // save to localStorage
-      const jobsJson = JSON.stringify(newJobs)
-      localStorage.setItem('job',jobsJson)
-      return newJobs;
-    })
-    setJob('')
-  } 
+  const [show,setShow] = useState(false)
   return (
-    <div className="App" style={{ padding: 20 }}>
-      <input value={job} onChange={e => setJob(e.target.value)}/>
-      <button onClick={handleSumit}>Send</button>
-      <ul>
-        {jobs.map((job, index) => (
-          <li key={index}>{job}</li>
-        ))}
-      </ul>
+    <div style={{ padding: 20 }}>
+      <button onClick={()=> {setShow(!show)}}>Show</button>
+     {show && <Content />}
     </div>
   );
   
---
+++ tiktok/src/Content.js
@@ -0,0 +1,6 @@
+function Content(){
+    return (
+        <h1>Hello anh em !!</h1>
+    )
+}
+export default Content;
